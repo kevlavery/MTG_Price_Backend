2144e4410994ed98fd250346e1b0222d455e8c1d
Kevin Lavery <k.lavery@fincad.com>
2018-05-29 15:32:13 -0700

    fixed bugs in token retrieval

--- utility/token.js
+++ utility/token.js
@@ -1,5 +1,5 @@
 var restCredentials = require('../data/TCGConnectionData.json');
-var request = require('request');
+var rPromise = require('request-promise-native');
 var Token = require('../models/token');
 
 const data = 'grant_type=client_credentials&client_id='
@@ -10,48 +10,49 @@ const data = 'grant_type=client_credentials&client_id='
 exports.getToken = async () => ***REMOVED***
     try ***REMOVED***
         let tokenQuery = await Token.findOne(***REMOVED******REMOVED***).exec();
-        if(tokenQuery !== null) ***REMOVED***
-            var expiry_date = new Date(tokenQuery.expiry_date);
-            if (expiry_date <= Date.now()) ***REMOVED***
-                console.log("in if" )
-                await getRESTToken();
-                tokenQuery = await Token.findOne(***REMOVED******REMOVED***).exec();
-                return tokenQuery.access_token;
+        
 
-            ***REMOVED***
-        ***REMOVED*** else ***REMOVED***
-            await getRESTToken();
-            tokenQuery = await Token.findOne(***REMOVED******REMOVED***).exec();
-            return tokenQuery.access_token;
+        //let expiry_date = new Date(tokenQuery.expiry_date);
 
+        if(!tokenQuery || new Date(tokenQuery.expiry_date) <= Date.now()) ***REMOVED***
+            tokenQuery = await getRESTToken();
         ***REMOVED***
+
+
+        // if(tokenQuery) ***REMOVED***
+        //     var expiry_date = new Date(tokenQuery.expiry_date);
+        //     if (expiry_date <= Date.now()) ***REMOVED***
+        //         tokenQuery = await getRESTToken();
+        //     ***REMOVED***
+        // ***REMOVED*** else ***REMOVED***
+        //     tokenQuery = await getRESTToken();
+        // ***REMOVED***
+        return tokenQuery.access_token;
     ***REMOVED*** catch (error) ***REMOVED***
         console.log(error);
     ***REMOVED***
-    return tokenQuery.access_token;
-
 ***REMOVED***
 
 const getRESTToken = () => ***REMOVED***
-    return request(***REMOVED***
+   return rPromise(***REMOVED***
         url: "http://api.tcgplayer.com/token", //look to see if they support HTTPS
         method: "POST",
         headers: ***REMOVED***
             "Content-Type": "application/json"  
         ***REMOVED***,
         body: data
-    ***REMOVED***, (error, response, body) => ***REMOVED***
-        console.log('error getting token:', error);
-        let token = JSON.parse(body);
+    ***REMOVED***).then((tokenQuery) => ***REMOVED***
+        let token = JSON.parse(tokenQuery);
         let currentToken = new Token(***REMOVED***
             access_token: token.access_token,
             expiry_date: token['.expires']
         ***REMOVED***);
-        
         currentToken.save((err) => ***REMOVED***
             if (err) console.log(err);
         ***REMOVED***);
+        return token;
     ***REMOVED***);
+    
 ***REMOVED***
 
 
