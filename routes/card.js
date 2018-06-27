32e357914f7b9776452fad3cd192cfebcde2d748
Kevin Lavery <k.lavery@fincad.com>
2018-06-27 11:37:36 -0700

    added sets endpoint

---
+++ routes/card.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) ***REMOVED***
+    cardId = req.params.id;
+    
+
+    res.status(200).send(users);
+***REMOVED***);
+
+module.exports = router;
--- routes/sets.js
+++ routes/sets.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Sets = require('../models/sets');
 
 /* GET home page. */
-router.get('/', function(req, res, next) ***REMOVED***
-    var users = [];
-    users.push(***REMOVED***
-        firstName: "Kevin",
-        lastName: "Lavery",
-        username: "kevlavery",
-        email: "email addy"
-     ***REMOVED***);
-
-    res.status(200).send(users);
+router.get('/:name', function(req, res, next) ***REMOVED***
+    var name = req.params.name;
+    console.log(req.params);
+    Sets.findOne(***REMOVED***"name": name***REMOVED***).exec((err, set) => ***REMOVED***
+        res.status(200).send(set);
+        console.log("result: "+set);
+        console.log("error: "+err);
+    ***REMOVED***);
 ***REMOVED***);
 
 module.exports = router;
