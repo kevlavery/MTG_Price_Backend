088129a9c431664bc277864504489f81ae502417
Kevin Lavery <kevlavery@gmail.com>
2019-08-08 06:29:14 -0700

    added back memory and time monitoring

--- utility/updateCardPrices.js
+++ utility/updateCardPrices.js
@@ -10,8 +10,13 @@ mongoose.connect(databaseConnection.url, function(err)***REMOVED***
 ***REMOVED***);
 mongoose.set('useCreateIndex', true);
 
+var t0 = performance.now();
 CardTools.updateCardPriceStream()
 .then(() => ***REMOVED***
+    var t1 = performance.now();	
+    console.log(`It took $***REMOVED***((t1-t0)/1000).toFixed(2)***REMOVED*** seconds to update card prices`);	
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;	
+    console.log(`The script uses approximately $***REMOVED***used***REMOVED*** MB`);
     mongoose.disconnect();
 ***REMOVED***);
 
